d7eb0a26cbac7242f0a5fa23fba7d98a3ada7254
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2020-11-10 15:44:41 +0000

    First draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # create a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # read an individual task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  patch 'tasks/:id', to: 'tasks#update', as: :tasks_update
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
